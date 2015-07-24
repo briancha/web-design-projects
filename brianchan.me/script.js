769278a9afaeb65fe6900b5711f1fe5ffcae9aa6
Brian <briancha@users.noreply.github.com>
2015-07-24 10:19:13 -0700

    Add more books

--- brianchan.me/script.js
+++ brianchan.me/script.js
@@ -3,12 +3,12 @@
 // adds link to image source on that image
 // $('.java-assignments img').wrap('<a href=' + $('.row img').attr('src') + '></a>'); 
 
-// adds target="_blank" to all external links
+// adds target=_blank to all external links
 $('a[href^="http"]').attr({ 
 	target: "_blank" 
 });
 
-// add target="_blank" to web design projects
+// add target=_blank to web design projects
 $('.web-design a').attr({ 
 	target: "_blank" 
 });
